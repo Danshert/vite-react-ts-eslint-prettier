934ca914d9df4192b6cd023d0b67fdac287c4385
Danshert <danielht97-@outlook.com>
2022-07-13 20:03:59 -0500

    vite-plugin-pwa installed and pwa settings configured.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		VitePWA({
+			injectRegister: 'auto',
+			registerType: 'autoUpdate',
+			workbox: {
+				clientsClaim: true,
+				skipWaiting: true,
+				globPatterns: ['**/*{js,css,html,ico,png,svg}'],
+			},
+			includeAssets: [
+				'logo.svg',
+				'apple-touch-icon.png',
+				'masked-icon.svg',
+			],
+			manifest: {
+				name: 'My Awesome App',
+				short_name: 'MyApp',
+				description: 'My Awesome App description',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+		}),
+	],
 });
